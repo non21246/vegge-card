60039ff258e45056164c62c03afcdc1fc0404eb7
Turanon Hothong <turanon.hoth@northbkk.ac.th>
2024-01-21 14:05:05 +0700

    update 21/01/2024

--- src/componants/Homepage.js
+++ src/componants/Homepage.js
@@ -1,18 +1,11 @@
 import { Link } from 'react-router-dom';
-import useSound from 'use-sound';
-import { useEffect } from 'react';
-
 import '../App.css'
-import meme from '../assets/sounds/toothless.m4a'
+
 const Homepage = () => {
-    const [play, { pause }] = useSound(meme, { loop: true })
-    useEffect(() => {
-        play();
-      }, [play]);
     return (
         <div className="homepage-container background">
             <div>
-            <Link to={`/play`}><button onClick={pause} className='button-center'>Start</button></Link>
+            <Link to={`/play`}><button className='button-center'>Start</button></Link>
             </div>
         </div>
     );
